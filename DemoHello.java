47a4f9866d425d554590bc23503cf4aab106a516
anil-webonise <anil.ingale@weboniselab.com>
2020-07-18 12:33:54 +0530

    first 3 program

---
+++ DemoHello.java
@@ -0,0 +1,8 @@
+class DemoHello{
+public static void main(String[] args){
+  System.out.println("Hello Vaishnavi...!!!");
+ System.out.println("Hello Anil...!!!");
+System.out.println("Hello Mangal...!!!");
+}
+
+}
---
+++ DemoMultiply.java
@@ -0,0 +1,18 @@
+class DemoMuitiply{
+public static void main(String[] args){
+
+System.out.println("20*40");
+System.out.println("20*60");
+System.out.println("20*80");
+System.out.println("20*100");
+System.out.println("20*120");
+System.out.println(20*140);
+}
+
+}
+
+// for compile
+javac DemoMuitiply.java  
+
+// for execute 
+ java DemoMuitiply
